7c3fa7023e04e043afc1f7f3c706de0265ee8961
WarrenRice <warren.rice.ca@gmail.com>
2023-11-02 21:24:57 -0300

    add port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-app.listen(3000, () => console.log("listening at 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening at ${port}`));
 
 app.use(express.static("public"));
